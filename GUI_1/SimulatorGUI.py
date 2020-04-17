9b6bd52d5e3fbe0a230c083a265ac1ee4909c3aa
jainp <pranavjain977@gmail.com>
2020-04-16 22:13:19 -0700

    pep8 format and docstrings for SimulatorGUI and sender.py
    Change-Id: Id386adc720aae382373dbe2b8ccac13befa8bf03

--- GUI_1/SimulatorGUI.py
+++ GUI_1/SimulatorGUI.py
@@ -1,13 +1,9 @@
-from tkinter import *
+import inspect
 import math
 import os
 import sys
-import inspect
+from tkinter import *
 from tkinter import ttk
-
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
 
 import sender
 
@@ -17,16 +13,6 @@ window.iconbitmap(r'carsensors.ico')
 
 window.title("Car Health Monitor")
 
-# width_value = (window.winfo_screenwidth() / 2)
-# height_value = (window.winfo_screenheight() / 2)
-
-# x = (window.winfo_screenwidth() // 15) - (width_value // 15)
-# y = (window.winfo_screenheight() // 10) - (height_value // 10)
-
-# window.geometry("%dx%d+%d+%d" % (width_value, height_value, x, y))
-
-# window.minsize(math.ceil(width_value), math.ceil(height_value))
-# window.maxsize(math.ceil(width_value), math.ceil(height_value))
 window.minsize(800, 600)
 window.maxsize(800, 600)
 window.configure(bg='black')
@@ -42,17 +28,17 @@ email = "ABC@email.com"
 fontstyle = "Helvetica"
 small_fsize = 13
 large_fsize = 15
+
 combostyle = ttk.Style()
 combostyle.theme_create('combostyle', parent='alt',
                         settings={'TCombobox':
-                                      {'configure':
-                                           {'selectbackground': 'black',
-                                            'fieldbackground': 'black',
-                                            'background': 'dark grey',
-                                            'arrowcolor': 'black',
-                                            'bordercolor': 'white'
-
-                                            }}})
+                                  {'configure':
+                                   {'selectbackground': 'black',
+                                    'fieldbackground': 'black',
+                                    'background': 'dark grey',
+                                    'arrowcolor': 'black',
+                                    'bordercolor': 'white'
+                                    }}})
 combostyle.theme_use('combostyle')
 
 # Initializing an object of sender
@@ -61,53 +47,70 @@ rabbit_mq = sender.RabbitMq(queue='Hello',
                             exchange='',
                             routing_key='Hello')
 
-x = 0
+# x = 0
 sensor1Data = []
 sensor2Data = []
 
-# Main title
-lbl_Title = Label(window, text="\n Sensor Data \n", bg="black", fg="white", font=(large_fsize, large_fsize))
+# Main titles
+lbl_Title = Label(window, text="\n Sensor Data \n", bg="black",
+                  fg="white", font=(large_fsize, large_fsize))
 
-lbl_Temp = Label(window, text="Engine Temperature (°C)", font=(fontstyle, small_fsize), bg="black", fg="white")
-# lbl_blank1 = Label(window, text=" ", bg="black")
-lbl_Press = Label(window, text="Tire Pressure (psi)", font=(fontstyle, small_fsize), bg="black", fg="white")
+lbl_Temp = Label(window, text="Engine Temperature (°C)", font=(
+    fontstyle, small_fsize), bg="black", fg="white")
+
+lbl_Press = Label(window, text="Tire Pressure (psi)", font=(
+    fontstyle, small_fsize), bg="black", fg="white")
 lbl_blank2 = Label(window, text="  ", bg="black")
 
-lbl_cat1 = Label(window, text="Car Make", font=(fontstyle, small_fsize), bg="black", fg="white")
+lbl_cat1 = Label(window, text="Car Make", font=(
+    fontstyle, small_fsize), bg="black", fg="white")
 lbl_blank3 = Label(window, text="  ", bg="black")
-lbl_cat2 = Label(window, text="Ambient Driving Condition", font=(fontstyle, small_fsize), bg="black", fg="white")
+
+lbl_cat2 = Label(window, text="Ambient Driving Condition",
+                 font=(fontstyle, small_fsize), bg="black", fg="white")
 lbl_blank4 = Label(window, text="  ", bg="black")
 
 lbl_heading = Label(window, text="\nTime elapsed after last replacement\n ", bg="black", fg="white",
                     font=(fontstyle, large_fsize))
 lbl_blank5 = Label(window, text="   ", bg="black")
 
-lbl_oilTime = Label(window, text="For Oil (in days)", font=(fontstyle, small_fsize), bg="black", fg="white")
+lbl_oilTime = Label(window, text="For Oil (in days)", font=(
+    fontstyle, small_fsize), bg="black", fg="white")
 lbl_blank3 = Label(window, text="           ", bg="black")
-lbl_tireTime = Label(window, text="For Tire (in months)", font=(fontstyle, small_fsize), bg="black", fg="white")
+
+lbl_tireTime = Label(window, text="For Tire (in months)", font=(
+    fontstyle, small_fsize), bg="black", fg="white")
 lbl_blank6 = Label(window, text="  \n ", bg="black")
 lbl_blank7 = Label(window, text="  \n ", bg="black")
 
-email_input = Entry(window, textvariable=mailID, bg="dark grey", font=(fontstyle, large_fsize), fg="black")
+email_input = Entry(window, textvariable=mailID, bg="dark grey",
+                    font=(fontstyle, large_fsize), fg="black")
 lbl_blank8 = Label(window, text="        \n   ", bg="black")
+
 lbl_email = Label(window, text="Please enter email id to receive notifications", font=(fontstyle, small_fsize),
                   bg="black", fg="white")
 
-count_Temp = Label(window, text=temp, bg="black", fg="white", font=(fontstyle, small_fsize))
-count_Press = Label(window, text=press, bg="black", fg="white", font=(fontstyle, small_fsize))
-count_oil_time = Label(window, text=oil_time_days, bg="black", fg="white", font=(fontstyle, small_fsize))
-count_tire_time = Label(window, text=tire_time_months, bg="black", fg="white", font=(fontstyle, small_fsize))
+# Number labels
+count_Temp = Label(window, text=temp, bg="black",
+                   fg="white", font=(fontstyle, small_fsize))
+count_Press = Label(window, text=press, bg="black",
+                    fg="white", font=(fontstyle, small_fsize))
+count_oil_time = Label(window, text=oil_time_days,
+                       bg="black", fg="white", font=(fontstyle, small_fsize))
+count_tire_time = Label(window, text=tire_time_months,
+                        bg="black", fg="white", font=(fontstyle, small_fsize))
+
 
 makes = ['A', 'B', 'C', 'D']
 make_select = ttk.Combobox(window, values=makes, width=0)
 models = ['100D', '200D', '300D', '400D']
 model_select = ttk.Combobox(window, values=models, width=0)
 
-# grid layout
+# Placing Labels using grid function
+# Text Labels
 lbl_Title.grid(row=0, column=1, sticky=N + S + E + W, columnspan=3)
 
 lbl_Temp.grid(row=4, column=0, sticky=N + S + E + W)
-# lbl_blank1.grid(row=5, column=0, sticky=N + S + E + W)
 
 lbl_Press.grid(row=6, column=0, sticky=N + S + E + W)
 lbl_blank2.grid(row=7, column=0, sticky=N + S + E + W)
@@ -131,6 +134,7 @@ email_input.grid(row=18, column=2, sticky=N + S + E + W)
 lbl_email.grid(row=18, column=0, sticky=N + S + E + W, columnspan=1)
 lbl_blank8.grid(row=19, column=4, sticky=N + S + E + W)
 
+#Number Labels
 count_Temp.grid(row=4, column=2, sticky=N + S + E + W)
 count_Press.grid(row=6, column=2, sticky=N + S + E + W)
 make_select.grid(row=8, column=2, sticky=N + S + E + W)
@@ -138,17 +142,21 @@ model_select.grid(row=10, column=2, sticky=N + S + E + W)
 count_oil_time.grid(row=14, column=2, sticky=N + S + E + W)
 count_tire_time.grid(row=16, column=2, sticky=N + S + E + W)
 
+# Reading image files 
+# image file used for + and - button
 img_increase = PhotoImage(file='increase.png')
 img_decrease = PhotoImage(file='decrease.png')
 
-
 def increase_temp():
+    """Function to increase temperature variable
+    """
     global temp
     temp = temp + 10
     count_Temp.configure(text=temp)
 
-
 def decrease_temp():
+    """Function to decrease temperature variable
+    """
     global temp
     if temp > 0:
         temp = temp - 10
@@ -156,6 +164,8 @@ def decrease_temp():
 
 
 def increase_press():
+    """Function to increase pressure variable
+    """
     global press
     if press < 40:
         press = press + 1
@@ -163,6 +173,8 @@ def increase_press():
 
 
 def decrease_press():
+    """Function to decrease pressure variable
+    """
     global press
     if press > 0:
         press = press - 1
@@ -170,37 +182,59 @@ def decrease_press():
 
 
 def increase_oilTime():
+    """Function  to  increase oil variable
+    Oil time represents time elapsed since
+    oil was replaced.
+    """
     global oil_time_days
     oil_time_days = oil_time_days + 1
     count_oil_time.configure(text=oil_time_days)
 
 
 def decrease_oilTime():
+    """Function to decrease oilTime variable
+    Oil time  represents  time elapsed since
+    oil was replaced.
+    """
     global oil_time_days
+    # Condition to limit the minimum value of the variable to 0
     if oil_time_days > 0:
         oil_time_days = oil_time_days - 1
         count_oil_time.configure(text=oil_time_days)
 
 
 def increase_tireTime():
+    """Function to increase tireTime variable
+    Tire time  represents  time elapsed since
+    tire was changed.
+    """
     global tire_time_months
     tire_time_months = tire_time_months + 3
     count_tire_time.configure(text=tire_time_months)
 
 
 def decrease_tireTime():
+    """Function to decrease tireTime variable
+    Tire time  represents  time elapsed since
+    tire was changed.
+    """
     global tire_time_months
+    # Condition to limit the minimum value of the variable to 0
     if tire_time_months > 0:
         tire_time_months = tire_time_months - 3
         count_tire_time.configure(text=tire_time_months)
 
 
 def transmit():
+    """Function  to set  the  value of email to 
+    the one entered by the user in the text box
+    """
     global mailID
     global email
     email = mailID.get()
 
 
+# Button to increment and decrement the parameters
 btn_decTemp = Button(window, image=img_decrease, command=decrease_temp,
                      bg="black", borderwidth=0, activebackground="black")
 btn_incTemp = Button(window, image=img_increase, command=increase_temp,
@@ -217,8 +251,10 @@ btn_decTireTime = Button(window, image=img_decrease, command=decrease_tireTime, 
                          activebackground="black")
 btn_incTireTime = Button(window, image=img_increase, command=increase_tireTime, bg="black", borderwidth=0,
                          activebackground="black")
-btn_mailID = Button(window, text="Enter", command=transmit, bg="black", fg="white", height=2, width=4)
+btn_mailID = Button(window, text="Enter", command=transmit,
+                    bg="black", fg="white", height=2, width=4)
 
+# Placing buttons using grid function
 btn_decTemp.grid(row=4, column=1, sticky=N + S + E + W)
 btn_incTemp.grid(row=4, column=3, sticky=N + S + E + W)
 btn_incPress.grid(row=6, column=1, sticky=N + S + E + W)
@@ -239,20 +275,19 @@ window.grid_rowconfigure(5, weight=4)
 
 
 def timer():
-    global x
+    # global x
 
-    if x < 10000:
-        window.after(1000, timer)  # call this function again in 1,000 milliseconds
+    # if x < 1100:
+        # call this function again in 1,000 milliseconds
+        window.after(1000, timer)
         print("Updating ...")
-        # time_text = "Timer: {} weeks".format(x)
-        # timer_display.configure(text=time_text)
 
-        # rabbit_mq.publish(payload=temp)
         sensor1Data.append(temp)
         sensor2Data.append(press)
 
         category1 = model_select.get()
         category2 = make_select.get()
+
         # store 10 reading in a list and then publish at once
         if len(sensor1Data) == 10:
             rabbit_mq.publish(payload={"sensor1Data": sensor1Data,
@@ -264,12 +299,11 @@ def timer():
                                        "category2": category2})
             sensor2Data.clear()
             sensor1Data.clear()
-            # print(f"Value f x is{x}")
-            # to set an infinite loop to continuously send data
-            if x > 100:
-                x = 0
-        x += 1
 
+            # Set X=0 to run timer function in an infinite loop
+        #     if x > 1000:
+        #         x = 0
+        # x += 1
 
 timer()
 window.mainloop()
