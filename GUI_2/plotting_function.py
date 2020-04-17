afd794b7aab5d380311d2262d2b706b9fd1dc1de
jainp <pranavjain977@gmail.com>
2020-04-17 01:28:01 -0700

    pep8 formatting and docstrings for splotting_function.py and receiver.py| Chaged stating value of tire pressure in SimulatorGUI
    Change-Id: I4b7459e7d26c64a52b53d0d7091f7b17e1931562

--- GUI_2/plotting_function.py
+++ GUI_2/plotting_function.py
@@ -1,62 +1,43 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
+debug = False
 
-
-debug=False
 
 class dataManipulation():
     """
         This class is used to read a text file, use the data to calculate
         corresponding value of reliability and plot the graph
-
     """
 
     def __init__(self):
+        """Initialize the object of class dataManipulation
+        """
         self.x_values = []
         self.y_values = []
 
-
-    def computeX(self,time):
-        """
-            This method is used to read the data from the text file and
-            store it in a form of list
+    def computeX(self, time):
+        """This function is used to convert a value of time
+        into a discrete array of time from 0 to that number
+        for the purpose plotting the equation
 
         Arguments:
-            filename {[string]} -- [.txt file from which the time data is read]
-
-        Returns:
-            [list] -- [Data from the file stored in a list]
+            time {int} -- time to be entered in the equation
         """
-        
 
-        # with open(self.fileName, 'r') as filehandle:
-        #     for line in filehandle:
-        #         line.rstrip("\n")
-        #         # remove linebreak which is the last character of the string np.exp(-(i/5190)**1.55)
-        #         currentData = line[:-1]
-
-        #         # add item to the list
-        #         self.x_values.append(float(currentData))
-                
-
-        timeinc=0
+        timeinc = 0
 
         for i in range(40+1):
-            stepSize=time/40
-            
+            stepSize = time/40
+
             self.x_values.append(timeinc)
-            timeinc=timeinc+stepSize
-        # print(self.x_values)
-
-                    
-
-        
+            timeinc = timeinc+stepSize
 
     def computeY(self, formula):
         """
-            This method is used to calculate the value corresponding to x.
-            It will compute the equation based on the x value and the formula provided  
+        This method is used to calculate the value corresponding
+        to x vector. It will compute the equation based on the x
+        value and the formula provided.
 
         Arguments:
             formula {[string]} -- [Equation used to compute y]
@@ -64,39 +45,38 @@ class dataManipulation():
         Returns:
             [list] -- [The calculated y value is stored in a form of list]
         """
-
+        # i takes values of time from the x_values list
         for i in self.x_values:
             y = eval(formula)
+            # eval function evaluates the “String” like a python
+            # expression and returns the result as an integer
             self.y_values.append(y)
 
     def graph(self, x_label, y_label, graph_title):
         """
-        This function can be used to plot the graphs based on equation of the curve.
+        This function can be used to plot the
+        graphs based on equation of the curve.
 
-    Arguments:
-        x_label {[type]} -- [title of x axis]
-        y_label {[type]} -- [title of y axis]
-        graph_title {[type]} -- [title of graph]
-        """
+        Arguments:
+            x_label {[type]} -- [title of x axis]
+            y_label {[type]} -- [title of y axis]
+            graph_title {[type]} -- [title of graph]
+            """
         x = np.array(self.x_values)
-        # eval function evaluates the “String” like a python expression and returns the result as an integer
+
         y = self.y_values
-        plt.plot(x, y)  # used to plot the graphs
+        # Plotting the Graphs, X & Y Labels and title
+        plt.plot(x, y)
         plt.title(graph_title)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
         plt.show()
 
 
-if debug==True:
+# Code present incase we need to test the code above
+if debug is True:
     engineOilReliability = dataManipulation()
     engineOilReliability.computeX(18000)
     engineOilReliability.computeY("np.exp(-(i/5190)**1.55)")
     engineOilReliability.graph('Hours', 'Reliability',
-                            'Running hours vs Reliability')
-
-
-#     # # To plot engine-oil failure curve
-# engineOilFailure = Plotting()
-# engineOilFailure.graph('1-np.exp(-(x/5190)**1.55)', range(0, 15000, 1000),
-#           'Hours', 'Failure', 'Running hours vs Failure')
+                               'Running hours vs Reliability')
--- GUI_2/receiver.py
+++ GUI_2/receiver.py
@@ -1,5 +1,6 @@
 import csv
 import json
+
 try:
     import pika
 except Exception as e:
@@ -14,11 +15,14 @@ class RabbitMqConfig(object):
 
     def __init__(self, host='localhost', queue='Hello'):
         """
-            This method is used to initialaise the server name and queue from where the data is received
+            This  method  is used to initialise the server
+            name and queue from where the data is received
         """
         self.host = host
         self.queue = queue
-        # to clear the contents of a file or create the file if it doesn't already exist
+
+        # To clear the contents of a file or create
+        # the file if it doesn't already exist
         open('sensor1.txt', 'w').close()
         open('SensorData.CSV', 'w').close()
         open('TimeElapsed.CSV', 'w').close()
@@ -28,26 +32,31 @@ class RabbitMqServer(object):
     """
         Initialise the server properties.
         - Local host tells that we are connected to a broker on local machine
-          To connect to a broker on different machine we need to mention the
+          To connect to a broker on  different machine we need to mention the
           IP Address or its name
-        - Before sending we need to make sure the recipient queue exists.
-          If we send a message to non-existing location, RabbitMQ will just drop
-          the message.
+        - Before  sending  we  need  to  make sure the recipient queue exists.
+          If we send a message to  non-existing  location, RabbitMQ will just
+          drop the message.
     """
 
     def __init__(self, server):
         self.server = server
+        # Step1: Establish Connection with RabbitMQ server
         self._connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=self.server.host))
         self._channel = self._connection.channel()
+        # Step2: Create a queue called 'hello' where msg would be delivered
         self._tem = self._channel.queue_declare(queue=self.server.queue)
     message = {}
 
+    # Step3: Receiving message from queue requires
+    # subscribing a callback  function  to a queue.
     @staticmethod
     def callback(ch, method, properties, body):
         """
-            Whenever we receive a message, this callback function is called by the Pika library.
-            In our case this function will print on the screen the contents of the message.
+            Whenever we receive a message, this  callback  function is
+            called by the Pika library. In our case this function will
+            print on the screen the contents of the message.
         """
 
         message = body.decode("utf-8")
@@ -56,7 +65,8 @@ class RabbitMqServer(object):
         message = message.replace('\'', '\"')
         data = json.loads(message)
 
-        # iterating through the contents of a single packet sent from GUI and saving it in a file
+        # iterating through the contents of a single packet
+        # sent from GUI and saving it in a file
         for i in range(len(data['sensor1Data'])):
             with open('SensorData.CSV', 'a+', newline='') as f:
                 m = "{},{}\n".format(
@@ -76,9 +86,12 @@ class RabbitMqServer(object):
 
     def start_server(self):
         """
-            For that command to succeed we must be sure that a queue which we want to subscribe to exists.
-            Fortunately we're confident about that ‒ we've created a queue above ‒ using queue_declare.
+            For that command to succeed we must be sure that a queue which
+            we want to subscribe to  exists. Fortunately  we're  confident
+            about that ‒ we've created a queue above ‒ using queue_declare.
         """
+        # Step4:  Tell  RabbitMQ  that this particular callback
+        # function should receive messages from our hello queue
         self._channel.basic_consume(queue=self.server.queue,
                                     on_message_callback=self.callback,
                                     auto_ack=True)
